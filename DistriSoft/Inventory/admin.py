c9184a35476f59bc04d40f44e3ac6e964fd8e67e
CRICAPOGA <camilochoposada2@gmail.com>
2025-02-19 08:51:29 -0500

    Agregar modelos de Cotizacion y DetalleCotizacion en la aplicación de Inventario, y registrar en el admin.

--- DistriSoft/Inventory/admin.py
+++ DistriSoft/Inventory/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Categoria, Producto, Servicio
+from .models import Categoria, Producto, Servicio, Cotizacion, DetalleCotizacion
 
 # Register your models here.
 admin.site.register(Categoria)
 admin.site.register(Producto)
 admin.site.register(Servicio)
+admin.site.register(Cotizacion)
+admin.site.register(DetalleCotizacion)
