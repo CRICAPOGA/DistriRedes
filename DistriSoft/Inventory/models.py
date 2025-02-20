c9dc6acf0c8e16f42803f2fedebc1e76ed456a3a
CRICAPOGA <camilochoposada2@gmail.com>
2025-02-19 20:53:05 -0500

    Revert "Merge branch 'develop-Camila' into develop-Cristian"
    This reverts commit 23ce3d9e884a1481fb3e354fc2a1853a79078146, reversing
    changes made to dfcc5e59bdef6152f671408994c432fb91aaa59c.

--- DistriSoft/Inventory/models.py
+++ DistriSoft/Inventory/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+############ PRODUCTOS ############
 class Categoria(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
@@ -15,6 +15,16 @@ class Producto(models.Model):
     description = models.TextField()
     image = models.ImageField(upload_to='Inventory\static\img', null=True)
     category = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+############ SERVICIOS ############
+class Servicio(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
