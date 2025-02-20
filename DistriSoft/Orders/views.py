a07e4cbed5ee89d43ecdc424688888fc359924c4
CRICAPOGA <camilochoposada2@gmail.com>
2025-02-19 23:12:24 -0500

    Agregar vista y plantilla para gestionar pedidos, incluyendo lógica de estado y rutas (NO ES FUNCIONAL)

--- DistriSoft/Orders/views.py
+++ DistriSoft/Orders/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
+def pedidos(request):
+    return render(request, 'pedidos.html')
